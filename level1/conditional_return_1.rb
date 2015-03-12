1ebe934c1b8f6a24d4e4d8f32d0362737185b490
Armando Amador <arm.amador@gmail.com>
2015-03-12 01:42:36 -0400

    Use a conditional return on the if statement to refactor code

---
+++ level1/conditional_return_1.rb
@@ -0,0 +1,24 @@
+# Clean up the code below to only set search_result once by using a
+# conditional return on the if statement.
+
+# search = "Contra"
+# games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
+# search_index = games.find_index(search)
+# if search_index
+#   search_result = "Game #{search} found: #{games[search_index]} at index #{search_index}."
+# else
+#   search_result = "Game #{search} not found."
+# end
+# puts search_result
+
+search = 'Contra'
+games = ['Super Mario Bros.', 'Contra', 'Metroid', 'Mega Man 2']
+search_index = games.find_index(search)
+
+search_result = if search_index
+  "Game #{search} found: #{games[search_index]} at index #{search_index}."
+else
+  "Game #{search} not found."
+end
+
+puts search_result
