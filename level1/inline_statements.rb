901aa49b329cda97cb7f7f65134b6561b5378318
Armando Amador <arm.amador@gmail.com>
2015-03-12 01:20:04 -0400

    Refactor code by using inline conditional

---
+++ level1/inline_statements.rb
@@ -0,0 +1,11 @@
+# Doing a full unless statement is sometimes too much. Refactor the method below
+# to use a single-line unless statement.
+
+# games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
+# unless games.empty?
+#   puts "Games in your vast collection: #{games.count}"
+# end
+
+games = ['Super Mario Bros.', 'Contra', 'Metroid', 'Mega Man 2']
+
+puts "Games in your vast collection: #{games.count}" unless games.empty?
