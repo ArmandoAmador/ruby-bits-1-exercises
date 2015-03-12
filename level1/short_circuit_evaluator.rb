8951647891d88ebe0e1b481bf887a88bdf4c0db2
Armando Amador <arm.amador@gmail.com>
2015-03-12 01:54:49 -0400

    Us short-circuit evaluation instead of the if conditional statement

---
+++ level1/short_circuit_evaluator.rb
@@ -0,0 +1,18 @@
+# Using Short-Circuit Evaluation can clean up your code a great deal. Update
+# the following method to use short circuit evaluation. While you're at it,
+# why not try reducing the entire method to one line?
+
+# def search_index(games, search_term)
+#   search_index = games.find_index(search_term)
+
+#   if search_index
+#     search_index
+#   else
+#     "Not Found"
+#   end
+# end
+
+def search_index(games, search_term)
+  search_index = games.find_index(search_term)
+  search_index || "Not Found"
+end
