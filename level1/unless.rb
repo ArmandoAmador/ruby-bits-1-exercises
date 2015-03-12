5bfa9f22a02dbdf5edfbd7bb4bd42606e95e7460
Armando Amador <arm.amador@gmail.com>
2015-03-12 01:15:33 -0400

    use unless conditional

--- level1/unless.rb
+++ level1/unless.rb
@@ -13,5 +13,5 @@
 games = ['Super Mario Bros.', 'Contra', 'Metroid', 'Mega Man 2']
 
 unless games.empty?
-    puts "Games in your vast collection: #{games.count}"
+  puts "Games in your vast collection: #{games.count}"
 end
